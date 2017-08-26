ef16de52b3b77aa537b03a99cec29f5258a0f045
dha <dave.anker@gmail.com>
2017-08-26 16:47:00 +1000

    a

--- test.R
+++ test.R
@@ -1,25 +1,24 @@
-install.packages("rgdal")
 library(rgdal)
-library(ggmap)
 library(dplyr)
-data<-readOGR(dsn="C:/Users/David Anker/Documents/MDSI/36103 Statistical Thinking for Data Science/GBR/zipfolder",layer="Great_Barrier_Reef_Features")
-cays <- subset(data, data$FEAT_NAME == "Cay")
-sand <- subset(data,data$FEAT_NAME == "Sand")
-lon <- cays$X_COORD
-lat <- cays$Y_COORD
-data2 <- list("lon"=lon, "lat"=lat)
-data3 <- as.data.frame(data2)
-data4 <- data3 %>% mutate(name=cays$LOC_NAME_L)
+library(ggplot2)
+library(ggmap)
+library(ggrepel)
 
-gbr <- c(lon=149.3, lat=-19.5)
-gbr_map <- get_map(location=gbr, zoom=8, maptype="terrain")
-ggmap(gbr_map)+
-  geom_point(aes(lon, lat), colour="red", data=data3) +
-  geom_text(aes(label=lon), hjust=0, vjust=1.5, data=data4) +
-  geom_text(aes(label=lat), hjust=0, vjust=0.5, data=data4)
+data<-readOGR(dsn="C:/Users/ankerd/Desktop/MDSI/zipfolder",layer="Great_Barrier_Reef_Features")
 
-str(data)
-summary(data)
+data2 <- list("index"=1:nrow(data), "feat"=data$FEAT_NAME, "name"=data$GBR_NAME, "lon"=data$X_COORD, "lat"=data$Y_COORD, "area"=data$Area_HA)
+df <- as.data.frame(data2)
 
+cays <- subset(df, feat=="Cay")
+cays2 <- cays %>% filter(name!="U/N Cay")
+sand <- subset(df, feat=="Sand")
+reef <- subset(df, feat=="Reef")
+bugatti <- subset(df, name=="Bugatti Reef")
 
-cars<-mtcars
+gbr <- ggmap(get_googlemap(c(lon=149.41, lat=-19.72), zoom=8, maptype="hybrid", size=c(600,320)))
+
+gbr+
+  geom_point(aes(x=lon, y=lat), data=cays, colour="red") +
+  geom_point(aes(x=lon, y=lat), data=bugatti, colour="yellow") +
+  geom_text_repel(aes(label=name), data=bugatti, colour="white", size=3) +
+  geom_text_repel(aes(label=index), data=cays, colour="white", size=3)
